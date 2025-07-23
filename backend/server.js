8f92f1e242422027b627d2864e525d7d8ec3b91b
Chetan Sharma <chetansh0301@gmail.com>
2025-07-23 22:41:57 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+    res.send('Phone Book Backend is running');
+});
+
+// Connect to MongoDB & start server
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch(err => console.log(err));
